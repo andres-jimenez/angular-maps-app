a5efdd1f396d72a90819778d6fa259a8ff6bd565
Andres Jimenez <andres.jimenez@pragma.com.co>
2023-05-16 17:07:55 -0500

    implements properties page

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: [],
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {}
+export class MiniMapComponent implements AfterViewInit {
+  @ViewChild('map') mapContainer?: ElementRef;
+
+  @Input() lngLat?: [number, number];
+
+  ngAfterViewInit(): void {
+    if (!this.mapContainer) return;
+
+    if (!this.lngLat) throw "lngLat can't be nule";
+
+    const map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
+}
